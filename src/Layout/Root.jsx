95c88bda4f91e73c82748858a8d7c754b9017ad8
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-03 18:40:57 +0600

    Project Set Up Done

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+
+const Root = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Root
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>This is Navbar</h1>
+        </div>
+    )
+}
+
+export default Nav
