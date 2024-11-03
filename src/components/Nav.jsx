9009b740312c6c0833f852121aaa2a94df70892d
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-03 19:06:44 +0600

    Navbar Done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,40 @@
+import { NavLink } from "react-router-dom"
+import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
 const Nav = () => {
     return (
-        <div>
-            <h1>This is Navbar</h1>
-        </div>
+        <header className="max-w-[90%] xl:max-w-[1200px] mx-auto bg-purple-600 mt-6">
+            <div className="flex justify-between items-center text-white py-5 border border-red-500">
+                <div>
+                    <h1 className="text-3xl font-bold flex gap-0"><span>Gadget</span> <span className="text-black">Heaven</span></h1>
+                </div>
+
+                <div>
+                    <nav>
+                        <ul className="flex gap-8 font-semibold text-xl">
+                            <NavLink>Home</NavLink>
+                            <NavLink>Statistics</NavLink>
+                            <NavLink>Dashboard</NavLink>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div className="flex gap-5 text-black font-bold">
+                    <div className="bg-white h-8 w-8 flex items-center justify-center rounded-full">
+                        <div>
+                            <IoCartOutline className="mx-auto text-xl" />
+                        </div>
+                    </div>
+
+                    <div className="bg-white h-8 w-8 flex items-center justify-center rounded-full">
+                        <div>
+                            <MdOutlineFavoriteBorder className="mx-auto text-xl" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
