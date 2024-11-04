bbc010dd18d9755eecea4a801a923396032fe092
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-04 19:07:06 +0600

    All Product Button Perfectly Work

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -14,18 +14,20 @@ const Categories = () => {
 
     return (
         <div className="border border-gray-300 shadow-lg rounded-lg px-3 pt-3 mb-10 sm:mb-0">
-            <NavLink 
-                className={({isActive}) => `${isActive ? `bg-purple-600 block text-center px-12 py-3 rounded-3xl font-semibold mb-5` : `block bg-gray-300 mb-5 text-center px-12 py-3 rounded-3xl font-semibold hover:bg-purple-600`}`}
-                    >
+                <NavLink
+                    to='/'
+                    className={({isActive}) => `${isActive ? `bg-purple-600 block text-center px-12 py-3 rounded-3xl font-semibold mb-5` : `block bg-gray-300 mb-5 text-center px-12 py-3 rounded-3xl font-semibold hover:bg-purple-600`}`}
+                >
                 All Products
-            </NavLink>
+                </NavLink>
             {
-                categories.map(category => <NavLink 
+                categories.map(category => 
+                <NavLink 
                     to={`/category/${category.category}`}
                     className={({isActive}) => `${isActive ? `bg-purple-600 block text-center px-12 py-3 rounded-3xl font-semibold mb-5` : `block bg-gray-300 mb-5 text-center px-12 py-3 rounded-3xl font-semibold hover:bg-purple-600`}`}
                     key={category.product_id}>
                         {category.category}
-                    </NavLink>)
+                </NavLink>)
             }
         </div>
     )
